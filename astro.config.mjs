d44bb7e73b7a1818e69011feb69a0e750f93265a
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2025-03-27 13:17:37 +0000

    First stages of customisation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,11 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Knijn Wiki',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/EmmaKnijn/wiki',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			sidebar: [],
 		}),
 	],
 });
